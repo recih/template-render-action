a15635f717cf7824782e7a66859ab5ceee099bb1
recih <recih@users.noreply.github.com>
2022-01-21 03:21:33 +0800

    chore: use pnpm and fix test

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -2,7 +2,6 @@ import {wait} from '../src/wait'
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
-import {expect, test} from '@jest/globals'
 
 test('throws invalid number', async () => {
   const input = parseInt('foo', 10)
@@ -13,7 +12,7 @@ test('wait 500 ms', async () => {
   const start = new Date()
   await wait(500)
   const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
+  const delta = Math.abs(end.getTime() - start.getTime())
   expect(delta).toBeGreaterThan(450)
 })
 
