6d31c3d4a410ec9cf2cd6e28751ca6a3153aad9c
recih <recih@users.noreply.github.com>
2022-01-24 17:58:26 +0800

    test: add test for vars

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -17,15 +17,23 @@ afterEach(async () => {
 });
 
 test("test ejs template with glob mode", async () => {
+  const vars = {
+    name: "test",
+    version: "1.0.0",
+  };
   process.env["INPUT_TEMPLATE-FILE"] = "__tests__/**/*.ejs";
   process.env["INPUT_GLOB"] = "true";
   process.env["INPUT_ENGINE"] = "ejs";
+  process.env["INPUT_VARS"] = JSON.stringify(vars);
   const mainPath = path.join(__dirname, "..", "lib", "main.js");
   await execa.node(mainPath);
 
   // check if the action performed as expected
   const content = await fsPromises.readFile(path.join(__dirname, "../__tests__/test1.json"), "utf8");
   const outputData = JSON.parse(content);
+  expect(outputData.name).toBe(vars.name);
+  expect(outputData.version).toBe(vars.version);
+  expect(outputData.serverUrl).toBe("https://github.com");
   expect(outputData.serverUrl).toBe("https://github.com");
   expect(outputData.templateEngine).toBe(process.env["INPUT_ENGINE"]);
 });
